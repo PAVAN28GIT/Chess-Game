74517fbd6805210e568a1b1ceab2f2392f3d5850
PAVAN28GIT <pavankmjp@gmail.com>
2024-11-10 13:38:57 +0530

    fix : deployment error

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,6 @@
 const LocalURL = "http://localhost:8000";
 const ProductionURL = "https://chess-game-bslt.onrender.com/";
 
-export const BackendURL = process.env.NODE_ENV === 'production' ? ProductionURL : LocalURL;
+const BackendURL = process.env.NODE_ENV === 'production' ? ProductionURL : LocalURL;
+
+export default BackendURL;
