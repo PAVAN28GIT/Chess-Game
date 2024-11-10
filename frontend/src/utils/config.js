557fcaf4ad6235b100b004b635fa05f5657082b4
PAVAN28GIT <pavankmjp@gmail.com>
2024-11-10 13:44:40 +0530

    fix:backend url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 const LocalURL = "http://localhost:8000";
-const ProductionURL = "https://chess-game-bslt.onrender.com/";
+const ProductionURL = "https://chess-game-bslt.onrender.com";
 
 const BackendURL = process.env.NODE_ENV === 'production' ? ProductionURL : LocalURL;
 
